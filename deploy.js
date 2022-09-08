db61d7be028e45b28948c5446be198dc0a03bf9f
Ishrani Rajiv <rajivishrani@gmail.com>
2022-09-08 12:30:20 +0530

    code snippets

--- deploy.js
+++ deploy.js
@@ -1,7 +1,19 @@
 const ethers = require("ethers");
+const fs = require("fs-extra");
+// const fs = require("fs");
 
 async function main() {
-  const provider = new ethers.providers.JsonRpcProvider("");
+  const provider = new ethers.providers.JsonRpcProvider(/* "rpc url" */);
+  const wallet = new ethers.Wallet(/* "private key" */,
+  provider
+  );
+  const abi = fs.readFileSync(/* "abi file path" */,"utf8");
+  const binary = fs.readFileSync(/* "binary file path" */,
+  "utf8");
+  const contractFactory = new ethers.ContractFactory(abi, binary, wallet);
+  console.log("Deploying, please wait...");
+  const contract = await contractFactory.deploy(); //STOP here! Wait for contract to deploy...
+  console.log(contract);
 }
 
 main()
